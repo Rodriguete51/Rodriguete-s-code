923762266eb8f2e7f93f0a68bb6ecbd20fee1846
Rodriguete51 <142893967+Rodriguete51@users.noreply.github.com>
2023-08-29 08:41:54 -0300

    Add files via upload

--- Volume_Lata.c
+++ Volume_Lata.c
@@ -1,11 +1,1 @@
-#include<stdio.h>
-#include<math.h>
-float main()
-{
-    float V, Pi, R, A;
-    scanf("%f",&R);
-    scanf("%f",&A);
-    Pi = 3.14;
-    V = Pi * pow(R,2) * A;
-    printf("%f",V);
-}
+
