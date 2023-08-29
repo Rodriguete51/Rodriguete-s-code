9689c370847af997ed43cb74725de7719f678c74
Rodriguete51 <142893967+Rodriguete51@users.noreply.github.com>
2023-08-29 08:43:00 -0300

    Add files via upload

---
+++ Viagem.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+float main()
+{
+    float T, V, D, Lu;
+    scanf("%f",&T);
+    scanf("%f",&V);
+    scanf("%f",&D);
+    Lu = D/12;
+    printf("T:%f ",T);
+    printf("V:%f ",V);
+    printf("D:%f ",D);
+    printf("Lu:%f ",Lu);
+}
