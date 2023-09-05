e12df8ad302fd5046ae7ed9d2c9ce5cd56933fe6
Rodriguete51 <142893967+Rodriguete51@users.noreply.github.com>
2023-09-05 07:30:04 -0300

    Update Volume_Lata.c

--- Volume_Lata.c
+++ Volume_Lata.c
@@ -1,1 +1,13 @@
+#include<stdio.h>
+#include<math.h>
 
+float main()
+{
+    float V, A, R;
+    printf("Digite a area: ");
+    scanf("%f",&A);
+    printf("Digite o raio: ");
+    scanf("%f",&R);
+    V = 3.14 * pow(R,2) * A;
+    printf("O volume da lata e de: %f" ,V);
+}
