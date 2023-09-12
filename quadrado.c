137232a187b2a204a9ed3689d876c271e5f382e5
Rodriguete51 <142893967+Rodriguete51@users.noreply.github.com>
2023-09-12 07:39:24 -0300

    Add files via upload

---
+++ quadrado.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+
+float main()
+{
+    float A, V;
+    printf("digite um numero: ");
+    scanf("%f", &A);
+    V = pow(A,2);
+    printf("o quadrado desse numero e de: %f", V);
+}
