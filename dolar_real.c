1e0e8202bb2e83632b23eabba3dbd41764445c13
Rodriguete51 <142893967+Rodriguete51@users.noreply.github.com>
2023-09-12 08:10:49 -0300

    Update dolar_real.c

--- dolar_real.c
+++ dolar_real.c
@@ -6,7 +6,7 @@ float main()
     float R, C, D;
     printf("digite o valor em dolar: ");
     scanf("%f",&D);
-    printf("digite o valor da cotacao: ");
+    printf("digite o valor da cotacao do dolar: ");
     scanf("%f",&C);
     R = C * D;
     printf("o valor em real sera de: %f",R);
